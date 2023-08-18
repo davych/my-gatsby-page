1f49fe7ec21d95d270f38e3290569de040f43e6b
davy_chen <davy_chen@epam.com>
2023-08-18 16:25:53 +0800

    add cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,8 @@ module.exports = {
     //   }
     // },
     "gatsby-plugin-mdx",
-    "gatsby-plugin-netlify-cms"
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-netlify"
     // {
     //   resolve: "gatsby-plugin-netlify-cms",
     //   options: {
